cbb819cb103b338f80e066cf102ec152bb653588
Jim Berg <jberg.ics81@gtalumni.org>
2024-01-25 10:58:01 -0500

    Initialize repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Print "Hello World!" using a trivial Python module that makes use of a docstring and __main__ fuction entry point.
---
+++ helloworld.py
@@ -0,0 +1,8 @@
+""" Print 'Hello World!' using a __main__ function entry point."""
+
+def main():
+    """ Print 'Hello World!' """
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
