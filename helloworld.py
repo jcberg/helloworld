0a3d0aefe2382c0057e020af182f262129ceb524
Jim Berg <jberg.ics81@gtalumni.org>
2024-01-25 17:05:20 -0500

    Add collection of CLI args

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,25 @@
-""" Print 'Hello World!' using a __main__ function entry point."""
+""" Print 'Hello World!' using a __main__ function entry point and collecting CLI args with argparse."""
+
+import argparse
 
 def main():
     """ Print 'Hello World!' """
-    print("Hello World!")
+
+    # Set defaults
+    greeting = "Hello"
+    recipient = "World"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--greeting", help="choose your own greeting (Default: 'Hello')")
+    parser.add_argument("--recipient", help="choose your own recipient (Default 'World')")
+    cli_args = parser.parse_args()
+    if cli_args.greeting:
+        greeting = cli_args.greeting
+    if cli_args.recipient:
+        recipient = cli_args.recipient
+
+    hello_message = greeting + " " + recipient
+    print(f'{greeting} {recipient}!')
 
 if __name__ == "__main__":
     main()
